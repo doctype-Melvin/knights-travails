55257f84cdbbb8d8e9555e25d1005b8b41a3455d
doctype-Melvin <doctype.melvin@gmail.com>
2022-10-04 14:15:19 +0200

    <JS> makePattern fn now creates table -- <CSS> sets table and cell dimensions -- creates checkerboard

--- src/board.js
+++ src/board.js
@@ -1,10 +1,28 @@
 export const Board = (()=>{
   const board = document.querySelector('.board')
   board.classList.add('board')
+
   const makePattern = () => {
     const table = document.createElement('table')
+    for (let i = 0; i < 8; i++) {
+      const row = document.createElement('tr')
+      for (let j = 0; j < 8; j++) {
+        const cell = document.createElement('td')
+        if ((i+j) % 2 === 0) {
+          cell.classList.add('cell')
+          cell.classList.add('white')
+          row.append(cell)
+        } else {
+          cell.classList.add('black')
+          cell.classList.add('cell')
+          row.append(cell)
+        }
+      }
+      table.append(row)
+    }
     board.append(table)
   }
+
   return {
     board,
     makePattern,
