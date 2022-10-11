e9904c1c3289cf5b6851b8583102b82439011693
doctype-Melvin <doctype.melvin@gmail.com>
2022-10-11 08:28:55 +0200

    interim clean up

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import './style.css'
-import {Board} from './board.js'
-import {Moves} from './moves.js'
+// Create N arrays of N length
+const board = (N) => {
+  return [...Array(N)].map((node) => [...Array(N)].map((node) => null))
+}
+const a = board(8)
+console.log(a)
+const b = new Map(a)
+console.log(b)
