74500723b43d527e9d5fc81092aa56b27aa35b54
doctype-Melvin <doctype.melvin@gmail.com>
2022-10-17 13:01:07 +0200

    <JS>addVertex sets keys as string - needed for get method

--- src/app.js
+++ src/app.js
@@ -10,12 +10,17 @@ const Graph = (size) => {
   const addVertex = (size) => {
     for (let x = 0; x < size; x++) {
       for (let y = 0; y < size; y++) {
-        list.set([x, y], [])
+        list.set([x, y].toString(), [])
       }
     }
   }
   // addEdge(v, w)
   const addEdge = (v, w) => {
+    // v represents the starting position
+    // w represents each legal move
+    // access v's array, push w
+    // access w's array, push v
+    //
     list.get(v).push(w)
     list.get(w).push(v)
   }
@@ -40,9 +45,9 @@ const Graph = (size) => {
     printGraph,
   }
 }
-
+// Directional moves
+const dx = [-2, -1, -2, -1, 2, 1, 2, 1]
+const dy = [-1, -2, 1, 2, -1, -2, 1, 2]
 const test = Graph(8)
 test.addVertex(8)
-console.log(test.list)
-// /// Next: addEdge method
-
+console.log(test.list.get('0,0'))
