dcae5e6a704e56527cc51139892eeb558e4b1bd4
doctype-Melvin <doctype.melvin@gmail.com>
2022-10-20 15:34:08 +0200

    <JS>adds iteration over board cells to access adjacency lists

--- src/app.js
+++ src/app.js
@@ -34,41 +34,20 @@ const KT = () => {
   }
 
   const findPath = (board, start, end) => {
-    const known = [] // All visited cells/nodes/vertices
-    const Q = [] // Queue to track discovered nodes
-    const origin = JSON.stringify(start) // Starting position
-    const target = JSON.stringify(end) // Target position
-    const steps = 0
-
-    Q.push(origin)
-
+    const Q = []
+    const visited = []
+    const target = JSON.stringify(end)
+    let adjacency = null
+    Q.push(JSON.stringify(start))
     while (Q.length > 0) {
       const current = Q.shift()
-      let adj = null // Store the adjacency list of current cell
+      visited.push(current)
 
-      if (current === target) {
-        if (steps > 1) {
-          console.log(`It takes ${steps} steps from ${start} to ${end}`)
-        }
-        console.log(`It takes ${steps} step from ${start} to ${end}`)
-        return
-      }
       for (const cell of board) {
         if (JSON.stringify(cell[0]) === current) {
-          adj = cell[1]
+          adjacency = cell[1].map((item) => JSON.stringify(item))
         }
-      } // end of loop
-
-      for (const i in adj) {
-        const move = adj[i]
-        if (JSON.stringify(move) === target) {
-          console.log(known)
-        }
-        if (!known.includes(JSON.stringify(move))) {
-          known.push(JSON.stringify(move))
-          Q.push(JSON.stringify(move))
-        }
-      } // end of loop
+      }
     }
   }
 
@@ -82,5 +61,5 @@ const KT = () => {
 const a = KT()
 a.makeBoard(8)
 a.makeCells(a.board, 8)
-a.findPath(a.board, [0, 0], [3, 3])
+a.findPath(a.board, [0, 0], [7, 7])
 console.log(a.board)
