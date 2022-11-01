1bba7b71baab43dfb3e9fe37d25434da852230c5
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-01 13:58:38 +0100

    <JS>finished console program

--- README.md
+++ README.md
@@ -5,25 +5,14 @@ You can find the assignment [here](https://www.theodinproject.com/lessons/javasc
 ## Goal
 Find the shortest path between `Start` and `Target`
 
-### Prerequisites
-A `graph` can represent a network and can be used to determin an optimal path between two `vertices`.
-This makes the graph the desired data structure to solve the Knight's travails problem.
-A chessboard will be represented in a `graph` data structure. A `graph` consists of a set of 
-`vertices` which are connected by `edges`. An `edge` is made up of a pair of `vertices`. 
-In this particular case the vertices are the cells of the chessboard. The edges are determined 
-by the legal moves the knight can make.
-[This article](https://jarednielsen.com/data-structure-graph-breadth-first-search/) was very helpful 
-for setting up this project.
-[GfG](https://www.geeksforgeeks.org/implementation-graph-javascript/)
+## Data Structure 
+To solve this problem I've utilized a graph data structure. The graph consists of connected vertices. 
+For the problem at hand, an unweighted and undirected graph is the fitting choice. 
+The edges between vertices (cells on a chessboard) are made up of legal moves the Knight can make.
 
-### Setup 
-First I've created the two dimensional board using two loops. First loop sets the x coordinate and the
-second sets the y coordinate. Both go into an array. Another empty array is created which will serve as 
-the adjacency list later on. Both elements (coordinates and empty adjacency list) go into an array,
-which itself represents a cell on the board. Essentially this creates the graph data structure. 
+## Algorithm
+The shortest path between `Start` and `Target` positions is found by using a breadth-first-search algorithm.
 
-Next I've created two arrays (dx & dy) that hold all the possible moves the knight can make. In another loop
-I create the actual edges between the cells. The legal moves now populate the adjacency list of the cell of origin.
-
-### Outcome
-This was the hardest problem I've worked on so far. 
+### Resources
+[This article](https://aquarchitect.github.io/swift-algorithm-club/Shortest%20Path%20(Unweighted)/) helped
+me with understanding how to track the distance between `Start` and `Target`. 
--- src/app.js
+++ src/app.js
@@ -1,85 +1,104 @@
-import './style.css'
+// Moves
+const dx = [-1, -2, -1, -2, 1, 2, 1, 2] // Moves the knight can
+const dy = [-2, -1, 2, 1, -2, -1, 2, 1] // go in either direction
 
-const knightsTravails = () => {
-  const dx = [-2, -1, -2, -1, 1, 2, 1, 2] // Possible moves
-  const dy = [-1, -2, 1, 2, 2, 1, -2, -1] // in either direction
-
-  const makeCell = (x, y) => { // Create cell object
-    const position = `${x},${y}` // Position as string
-    const array = [x, y] // Position as numbers in array
-    const list = [] // Adjacency list
-    return {
-      position,
-      array,
-      list,
-    }
-  }
-
-  const board = [] // Board array
-  for (let x = 0; x < 8; x++) {
-    for (let y = 0; y < 8; y++) {
-      board.push(makeCell(x, y)) // Create x*y cells on board
-    }
-  }
-
-  const isLegal = (x, y) => { // Check for legal move
-    return (x >= 0 && x <= 7 && y >= 0 && y <= 7 )
-  }
-
-  const makeEdges = () => { // Creates array of legal moves
-    for (const cell of board) {
-      for (let i = 0; i < dx.length; i++) {
-        let x = cell.array[0]
-        let y = cell.array[1]
-        x += dx[i]
-        y += dy[i]
-        if (isLegal(x, y)) {
-          cell.list.push(`${x},${y}`)
-        }
-      }
-    }
-    return board
-  }
-
-  const findPath = (start, end) => {
-    const origin = start
-    const target = end
-    const board = makeEdges()
-    const V = []
-    const Q = []
-    Q.push(origin)
-    V.push(origin)
-
-    while (Q.length > 0) {
-      const current = Q.shift()
-      let adjacency = null
-      console.log(current)
-      if (current === target) {
-        console.log(`Path from ${start} to ${end}`)
-        return
-      }
-      for (const cell of board) {
-        if (current === cell.position) {
-          adjacency = cell.list
-        }
-      }
-      for (const i in adjacency) {
-        const neigh = adjacency[i]
-        if (!V.includes(neigh)) {
-          V.push(neigh)
-          Q.push(neigh)
-        }
-      }
-    }
-  }
-
+const cellObj = (a, b) => { // Create obj with cell position
+  const position = `${a},${b}` // Pos as string
+  const node = [a, b] // Pos as node for printing path
+  const list = [] // node for adjacency list
+  const dist = 0 // Distance counter to track steps
+  const parent = null
   return {
-    board,
-    isLegal,
-    makeEdges,
-    findPath,
+    position,
+    node,
+    list,
+    dist,
+    parent,
   }
 }
 
-const app = knightsTravails()
-console.log(app.findPath('2,2', '5,5'))
+
+const board = [] // 2d node === chessboard
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    board.push(cellObj(i, j))
+  }
+}
+
+const isValid = (a, b) => { // Checks for valid cell position
+  return (a >= 0 && a <= 7 && b >= 0 && b <= 7)
+}
+
+const addMove = () => {
+  for (let z = 0; z < dx.length; z++) {
+    let x; let y
+    for (const cell of board) {
+      x = cell.node[0]
+      y = cell.node[1]
+      x = x+dx[z]
+      y = y+dy[z]
+      if (isValid(x, y)) {
+        cell.list.push(`${x},${y}`)
+      }
+    }
+  }
+}
+
+const makePath = (array) => {
+  const p = []
+  while (array.length > 0) {
+    const cell = array.shift()
+    p.unshift(cell.position)
+    if (cell.parent) array.push(cell.parent)
+  }
+  return p
+}
+
+const moveKnight = (start, end) => {
+  addMove() // Creates edges - legal moves in adjacency list of cell
+  let origin = null
+  let target = null
+  for (const cell of board) { // Loops through board cells
+    if (cell.position === start) {
+      origin = cell // Sets the starting cell obj
+    }
+    if (cell.position === end) {
+      target = cell // Sets the ending cell obj
+    }
+  }
+
+  const V = [start] // Visited node - holds strings
+  const Q = [origin] // Queue - holds objects
+  const P = [target] // Path from start to target
+
+
+  while (Q.length > 0) { // While queue is not empty
+    const current = Q.shift()
+
+    if (current.position === end) {
+      console.log('%cShortest path from', 'color:green; font-size: 18px;')
+      console.log(`%c${start} to ${end}`, 'color: orange; font-size: 18px')
+      console.log( `%cfound in ${current.dist} steps`, 'color: hotpink; font-size: 18px')
+      console.log('%cHere\'s the path...', 'color:lightblue; font-size: 18px')
+      makePath(P).forEach((step) => console.log(`%c${step}`, 'color:yellow; font-size: 18px'))
+      return
+    } else {
+      current.list.forEach((item) => { // Go thorugh adjacency list
+        if (!V.includes(item)) { // If cell hasn't been visited
+          V.push(item) // Push cell position to visited
+          board.filter((element) => { // Filter cell objects
+            if (element.position === item) { // by matching adjacent moves
+              element.dist = current.dist+1 // Increment distance
+              element.parent = current// Set parent obj
+              Q.push(element) // push cell objects to queue
+            }
+          })
+        }
+      })
+    }
+  }
+}
+
+
+moveKnight('0,0', '2,1')
+console.log(board)
